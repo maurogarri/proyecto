ba471fa35f5c1890797017be112a0c7ab24229aa
felipe iniesta <garridogomezmauro@gmail.com>
2024-04-17 15:22:06 +0200

    1commit

---
+++ bin/proyecto.dart
@@ -0,0 +1,5 @@
+import 'package:proyecto/proyecto.dart' as proyecto;
+
+void main(List<String> arguments) {
+  print('Hello world: ${proyecto.calculate()}!');
+}
---
+++ test/proyecto_test.dart
@@ -0,0 +1,8 @@
+import 'package:proyecto/proyecto.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
